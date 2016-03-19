556c53ab983abe5d2e4154b0c544744d8fad780e
wenhao <hnrain1004@gmail.com>
2016-03-19 08:23:25 +0800

    init activityDemo

---
+++ ActivityTest/app/src/main/java/com/leochin/activitytest/OtherActivity.java
@@ -0,0 +1,16 @@
+package com.leochin.activitytest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by wenhao on 3/19/16.
+ */
+public class OtherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+    }
+}
